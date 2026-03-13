f406289c29984be9b46fb976d76acd824de489d7
Nilesh Kumbhar <kumbharnilesh930@gmail.com>
2026-03-13 14:50:24 +0530

    New model folder add

---
+++ Models/Departments.cs
@@ -0,0 +1,10 @@
+﻿namespace Git_MVC_PRO.Models
+{
+    public class Departments
+    {
+        public int DepartmentsId { get; set; }
+        public string Name { get;set; }
+
+        public string Description { get;set;}
+    }
+}
