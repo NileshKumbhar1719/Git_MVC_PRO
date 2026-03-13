6f30fd82413fff8c353af22195ee98a8351fc3a5
Mahesh7775 <maheshmashal199@email.com>
2026-03-13 15:50:42 +0530

    requirmed add

--- Models/Departments.cs
+++ Models/Departments.cs
@@ -1,8 +1,13 @@
-﻿namespace Git_MVC_PRO.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Git_MVC_PRO.Models
 {
     public class Departments
     {
         public int DepartmentsId { get; set; }
+
+        [Required]
+        [MaxLength(100)]
         public string Name { get;set; }
 
         public string Description { get;set;}
