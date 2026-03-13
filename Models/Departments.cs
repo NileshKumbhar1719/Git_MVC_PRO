ec5118ca0d4c027e73825cda6af2e2d50e7b0122
Nilesh Kumbhar <kumbharnilesh930@gmail.com>
2026-03-13 14:56:09 +0530

    New class add Employees

--- Models/Departments.cs
+++ Models/Departments.cs
@@ -1,10 +1,15 @@
-﻿namespace Git_MVC_PRO.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Git_MVC_PRO.Models
 {
     public class Departments
     {
+        [Key]
         public int DepartmentsId { get; set; }
         public string Name { get;set; }
 
         public string Description { get;set;}
+
+        public List<Employees> Employees { get; set; }
     }
 }
