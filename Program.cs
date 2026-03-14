e60f77d956d47388b637029bb692accbbb75e5c1
Nilesh Kumbhar <kumbharnilesh930@gmail.com>
2026-03-14 12:09:43 +0530

    Add Header Department and Employees Layout

--- Program.cs
+++ Program.cs
@@ -36,7 +36,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=DEPARTMENTS}/{action=Index}/{id?}")
+    pattern: "{controller=Home}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
